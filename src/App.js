07c7d78016a3f0b33996707c28d2dc7549e4a315
Hxrsh <58227294+Hxrsh@users.noreply.github.com>
2023-06-07 00:26:47 +0530

    Version change
    npm version change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Balance from "./Balance/Balance.js"
-import Card from "./Components/Card"
-import ExpenseDetail from './Detail/ExpenseDetail/ExpenseDetail';
+import logo from "./logo.svg";
+import "./App.css";
+import Balance from "./Balance/Balance.js";
+import Card from "./Components/Card";
+import ExpenseDetail from "./Detail/ExpenseDetail/ExpenseDetail";
 
 function App() {
-  return (<Card><Balance></Balance>
-  <ExpenseDetail></ExpenseDetail></Card>
-  ); 
-  
+  return (
+    <Card>
+      <Balance></Balance>
+      <ExpenseDetail></ExpenseDetail>
+    </Card>
+  );
 }
 
 export default App;
